41a288bd6fec68dabe6a9412634c356d739df076
mistermocay <fahralathallahchandra24@gmail.com>
2026-08-18 10:51:34 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwh3Ju2yVH2XzjGdd6uAhXhv137tIVTss",
+  authDomain: "disposable-momment.firebaseapp.com",
+  projectId: "disposable-momment",
+  storageBucket: "disposable-momment.firebasestorage.app",
+  messagingSenderId: "838324414716",
+  appId: "1:838324414716:web:0bde58655b20b20f77cf79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
